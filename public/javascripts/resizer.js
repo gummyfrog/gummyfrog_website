2cf353f28c03e1a9b4d2277669ba0ed56d499a13
gummyfrog <neilsemail@me.com>
2018-01-07 20:41:18 -0500

    Resizer Polish

--- public/javascripts/resizer.js
+++ public/javascripts/resizer.js
@@ -1,27 +1,50 @@
 function listFiles() {
   var input = document.getElementById('filesToUpload');
   var list = document.getElementById('fileList');
+  var error = document.getElementById('resizeErrorText');
 
-  //empty list for now...
+
   while (list.hasChildNodes())
   {
-  	list.removeChild(list.firstChild);
+    list.removeChild(list.firstChild);
   }
+
+
+  if(!input.files) {
+    error.innerHTML = "Upload a file first!";
+  }
+
+
+
+  for (var i=0; i<input.files.length;i++) {
+    if (input.files[i].type != 'image/png' && input.files[i].type != 'image/jpeg') {
+      console.log(input.files[i].type);
+      error.innerHTML = "PNGs and JPEGs only, please.<br/>" + input.files[i].name + " is " + input.files[i].type;
+      return;
+    }
+  }
+
+
+  // clear of errors!
+
+
+  //empty list for now...
 
   //for every file...
   for (var x = 0; x < input.files.length; x++)
   {
     readURL(input.files[x], x)
   	//add to list
+
   	var li = document.createElement('li');
+    li.id = input.files[x].name;
   	li.innerHTML = 'File ' + (x + 1) + ':  ' + input.files[x].name;
   	list.append(li);
-
-
-
   }
 
+
 }
+
 
 function readURL(input, index) {
   console.log('Processing:')
@@ -35,18 +58,19 @@ function readURL(input, index) {
         .background(0xFFFFFFFF)
         .contain(1080, 1080)       // set greyscale
         .getBase64(Jimp.MIME_JPEG, function (err, src) {
+              var parent = document.getElementById(input.name);
               var encase = document.createElement("a");
-
               encase.href = src;
               encase.download = 'resized-' + index + '-' + input.name
               encase.target = "_blank";
 
-              resized.appendChild(encase);
+              parent.innerHTML = ("")
+              parent.appendChild(encase);
 
               var img = document.createElement("img");
-              img.style.width = "100px";
-              img.style.height = "100px";
-              console.log(src);
+              img.style.padding = "10px 20px";
+              img.style.width = "200px";
+              img.style.height = "200px";
               img.setAttribute("src", src);
               img.setAttribute("href", src);
               encase.appendChild(img);
