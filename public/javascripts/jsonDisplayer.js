c2bf43c3cf2c9867e4182dd154a4457be1f5b145
gummyfrog <neilsemail@me.com>
2018-06-27 23:22:43 -0400

    map

--- public/javascripts/jsonDisplayer.js
+++ public/javascripts/jsonDisplayer.js
@@ -5,7 +5,7 @@ var chartFormat = function(data, name) {
 
   var obj =
   {
-    type:"doughnut",
+    type:"pie",
     data: {
       labels:Object.keys(data).sort(function(a,b){return data[b]-data[a]}),
       datasets:[
@@ -19,6 +19,7 @@ var chartFormat = function(data, name) {
 
     options:
     {
+      cutoutPercentage: 30,
       maintainAspectRatio: false,
       title:
       {
@@ -45,6 +46,107 @@ var chartFormat = function(data, name) {
 
 }
 
+
+var timeFormat = function(data, name) {
+  var newData = Object.keys(data).map(i => data[i]);
+
+  var timeChartData = {
+  			labels: newData.map(obj=> moment.unix(obj.x).format('MMM/DDD hh:mm:ss')),
+  			datasets: [{
+  				label: 'Collected Tweets',
+          fill: 'start',
+          borderColor: '#192466',
+  				borderWidth: 1,
+  				data: newData
+  			}]
+
+  		};
+
+
+  var obj =
+  {
+    type:"line",
+    data: timeChartData,
+
+    options: {
+      maintainAspectRatio: false,
+      elements: {
+        line: {
+          tension: 0,
+        }
+      },
+      title:
+      {
+        display:false,
+        text:name,
+        fontSize:30
+      },
+      legend:
+      {
+        display: false
+      },
+      tooltips:
+      {
+        bodyFontSize:20,
+        positionMode:"nearest"
+      }
+		}
+  }
+
+  console.log(newData.map(obj=> obj.x));
+  return obj;
+
+}
+
+
+
+var barFormat = function(data, name) {
+  var newData = Object.keys(data).map(i => data[i]);
+
+  var barChartData = {
+  			labels: Object.keys(data),
+  			datasets: [{
+  				label: '',
+          borderColor: '#192466',
+  				borderWidth: 1,
+  				data: newData,
+          backgroundColor:palette('tol-dv', newData.length).map(color=>'#' + color).reverse()
+  			}]
+
+  		};
+
+
+  var obj =
+  {
+    type:"horizontalBar",
+    data: barChartData,
+
+    options: {
+      maintainAspectRatio: false,
+      title:
+      {
+        display:false,
+        text:name,
+        fontSize:30
+      },
+      legend:
+      {
+        display: false
+      },
+      tooltips:
+      {
+        bodyFontSize:20,
+        positionMode:"nearest"
+      }
+		}
+  }
+
+  console.log(newData.map(obj=> obj.x));
+  return obj;
+
+}
+
+
 function display(name, value) {
 
   if(!isNaN(name)) {
@@ -68,6 +170,12 @@ function display(name, value) {
         return('Types');
     case 'data':
       return('Data');
+    case 'temp_times':
+      return('Time Chart');
+    case 'times':
+      return('Time Chart');
+    case 'popular':
+      return('Popular Tweets')
 
     case '_query':
       return ('<query-item>Query:  <query-value>"'+value+'"</query-value></query-item>');
@@ -95,6 +203,14 @@ function display(name, value) {
 var chartsToRender = [];
 var dataSets = [];
 
+var timesToRender = [];
+var timeDataSets = [];
+
+var barsToRender = [];
+var barDataSets = [];
+
+
+
 function crawl(object, depth, originalKey) {
   if(depth > 20) {
     return "";
@@ -104,17 +220,59 @@ function crawl(object, depth, originalKey) {
 
 
   if(['emojis', 'words', 'hashtags', 'types'].includes(originalKey)) {
+    // var id =(originalKey + '-' + Math.random() + '-' + depth);
+    // var data = {};
+    // for(key in object) {
+    //   data[key] = object[key];
+    // }
+    // returnHTML += '<canvas id="'+ id + '" width="200px" height="100px"></canvas>'
+    //
+    // dataSets.push(data);
+    // chartsToRender.push(id);
+    // return returnHTML + '</div><bracket>}</bracket>';
+    // just pie chart code
+
     var id =(originalKey + '-' + Math.random() + '-' + depth);
     var data = {};
     for(key in object) {
       data[key] = object[key];
     }
-    returnHTML += '<canvas id="'+ id + '" width="50" height="15 0"></canvas>'
+
+    returnHTML += '<canvas id="'+ id + '" width="200px" height="100px"></canvas>'
+
     dataSets.push(data);
     chartsToRender.push(id);
+    returnHTML += '</div>';
+
+    returnHTML += '<div class="barContainer">';
+
+    var id =(originalKey + '-' + Math.random() + '-' + depth);
+    returnHTML += '<canvas id="'+ id + '" width="500px" height="250px"></canvas>'
+
+    barDataSets.push(object);
+    barsToRender.push(id);
+    return returnHTML + '</div><bracket>}</bracket>';
+
+  } else if(originalKey == 'times') {
+
+    returnHTML = '<div class="timeContainer">';
+    var id =(originalKey + '-' + Math.random() + '-' + depth);
+    returnHTML += '<canvas id="'+ id + '" width="500px" height="250px"></canvas>'
+    console.log('carth')
+    console.log(object)
+    timeDataSets.push(object);
+    timesToRender.push(id);
+    return returnHTML + '</div><bracket>}</bracket>';
+
+  }
+   else if(originalKey == 'popular') {
+
+    returnHTML = '<div class ="tweet", tweetID="'+ object +'"></div>'
+
     return returnHTML + '</div><bracket>}</bracket>';
 
   } else {
+
     returnHTML ='<button class="accordion depth'+depth+'">+</button> <br> <ol class="panel">'
 
     for(key in object) {
@@ -146,50 +304,73 @@ var jsonDisplay = function(container, data) {
 
 
   console.log(chartsToRender);
-  container.innerHTML += returnHTML;
+  container.innerHTML = returnHTML;
 }
 
 
+  var jsonData = $.ajax({
+    url: "https://rawgit.com/gummyfrog/frogJson/master/popularWorking.json",
+    dataType: "json",
+  }).done(function (data) {
+      moment().format();
+      var container = document.getElementById("jstree");
+      jsonDisplay(container, data);
+
+      for(c=0;c<chartsToRender.length;c++) {
+        new Chart(document.getElementById(chartsToRender[c]), chartFormat(dataSets[c], ''));
+      }
+
+      for(b=0;b<timesToRender.length;b++) {
+        // timeFormat(timeDataSets[b], '')
+        new Chart(document.getElementById(timesToRender[b]), timeFormat(timeDataSets[b], ''));
+      }
+
+      for(l=0;l<barsToRender.length;l++) {
+        // timeFormat(timeDataSets[b], '')
+        new Chart(document.getElementById(barsToRender[l]), barFormat(barDataSets[l], ''));
+      }
 
 
-var jsonData = $.ajax({
-  url: "https://rawgit.com/gummyfrog/frogJson/master/fopso.json",
-  dataType: "json",
-}).done(function (data) {
-    var container = document.getElementById("jstree");
-    jsonDisplay(container, data);
-    // new Chart(document.getElementById("words-chart"), chartFormat(data.data.words, 'Word data for ' + data._query));
-    // new Chart(document.getElementById("emojis-chart"), chartFormat(data.data.emojis, 'Emoji data for '  + data._query));
-    // new Chart(document.getElementById("hashtags-chart"), chartFormat(data.data.hashtags, 'Hashtag data for ' + data._query));
-    // new Chart(document.getElementById("type-chart"), chartFormat(data.data.types, 'Type data for ' + data._query));
+      window.onload = (function(){
 
-    for(b=0;b<chartsToRender.length;b++) {
-      new Chart(document.getElementById(chartsToRender[b]), chartFormat(dataSets[b], ''));
-    }
+        var tweet = document.getElementById("tweet");
+        var id = tweet.getAttribute("tweetID");
 
-
-
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
-            this.classList.toggle("active");
-
-            var child = this;
-            var g = 0;
-            while( (child = child.previousSibling) != null )
-              g++;
-            /* Toggle between hiding and showing the active panel */
-            var panel = this.parentNode.childNodes[g + 4];
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
+        twttr.widgets.createTweet(
+          id, tweet,
+          {
+            conversation : 'none',    // or all
+            cards        : 'hidden',  // or visible
+            linkColor    : '#cc0000', // default is blue
+            theme        : 'light'    // or dark
+          })
+        .then (function (el) {
+          el.contentDocument.querySelector(".footer").style.display = "none";
         });
-    }
 
-});
+      });
+
+      var acc = document.getElementsByClassName("accordion");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+              /* Toggle between adding and removing the "active" class,
+              to highlight the button that controls the panel */
+              this.classList.toggle("active");
+
+              var child = this;
+              var g = 0;
+              while( (child = child.previousSibling) != null )
+                g++;
+              /* Toggle between hiding and showing the active panel */
+              var panel = this.parentNode.childNodes[g + 4];
+              if (panel.style.display === "block") {
+                  panel.style.display = "none";
+              } else {
+                  panel.style.display = "block";
+              }
+          });
+      }
+
+  });
