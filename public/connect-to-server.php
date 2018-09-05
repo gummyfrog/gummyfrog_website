5a6afb34d57db0d3a8a984ce0ea01bcdfe465a0e
gummyfrog <neilsemail@me.com>
2018-09-04 20:22:51 -0400

    hello !

--- public/connect-to-server.php
+++
@@ -1,9 +0,0 @@
-<?php
-DEFINE ('DB_USER', 'tweetlinks');
-DEFINE ('DB_PSWD', 'Dd5d-D8I_afq');
-DEFINE ('DB_HOST', 'den1.mysql6.gear.host');
-DEFINE ('DB_NAME', 'that_name');
-
-$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
-
-?>
