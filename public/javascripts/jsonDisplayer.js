16503737aa110b71e8043ea6714faf8f6e6398d6
gummyfrog <neilsemail@me.com>
2018-10-06 12:00:21 -0400

    wello yellow

--- public/javascripts/jsonDisplayer.js
+++ public/javascripts/jsonDisplayer.js
@@ -307,7 +307,7 @@ function jsonDisplay(container, data) {
 
 
 var jsonData = $.ajax({
-  url: "https://rawgit.com/gummyfrog/frogJson/master/test.json",
+  url: "https://rawgit.com/gummyfrog/frogJson/master/develop.json",
   dataType: "json",
 }).done(function (data) {
 
