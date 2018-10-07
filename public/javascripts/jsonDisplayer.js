c2dada2705a869aa057c7531feddfbf68d9681f7
gummyfrog <neilsemail@me.com>
2018-10-07 19:10:16 -0400

    hello wello

--- public/javascripts/jsonDisplayer.js
+++ public/javascripts/jsonDisplayer.js
@@ -307,7 +307,7 @@ function jsonDisplay(container, data) {
 
 
 var jsonData = $.ajax({
-  url: "https://rawgit.com/gummyfrog/frogJson/master/develop.json",
+  url: "https://rawgit.com/gummyfrog/frogJson/master/aplme.json",
   dataType: "json",
 }).done(function (data) {
 
