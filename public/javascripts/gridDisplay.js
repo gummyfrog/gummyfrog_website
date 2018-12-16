cbc74115255026635bfb93a8a8761d0585cca0ce
gummyfrog <neilsemail@me.com>
2018-12-16 16:53:18 -0500

    a

--- public/javascripts/gridDisplay.js
+++ public/javascripts/gridDisplay.js
@@ -1,14 +1,3 @@
-function displayEmbed(data) {
-  var keys = Object.keys(data);
-  var returnHTML = '';
-  for(i=0;i<keys.length;i++) {
-    var key = keys[i];
-    returnHTML += embed(data[key], key, data[key]['last'], data[key]['desc'])
-  }
-
-  return(returnHTML)
-};
-
 function type(val) {
   if(val == "offline" || val == "online") {
     return(`<${val}>${val}</${val}>`);
@@ -16,7 +5,11 @@ function type(val) {
   return(`<${typeof(val)}>${val}</${typeof(val)}>`);
 }
 
+
 function embed(data, key, time, desc) {
+
+  var ret;
+
   var info = Object.keys(data).map((key) => {
     if(key == 'last' || key == 'desc') {
       return;
@@ -26,21 +19,62 @@ function embed(data, key, time, desc) {
   })
 
   console.log(info);
-  return(`
-    <div class="embed status" id="${key}">
 
-    <div class="titleBar">
-      <span class="titleText">${key}</span>
-      <span class="infoText">⏰ ${time}</span>
-    </div>
+  if(document.getElementById(key)) {
+    // this embed already exists...
+    console.log('This embed already exists...')
 
-    <div class="desc">
-      <span>${desc}</span>
-    </div>
+    console.log(document.getElementById(`${key}-content`));
+    console.log(`Info: ${info}`);
+    ret = document.getElementById(key).outerHTML;
 
-    <div class="embedContent">${info.join('\n')}</div>
-    </div>`)
+    if(document.getElementById(`${key}-content`).innerHTML == info.join('\n')) {
+      console.log('No change.');
+    } else {
+      setInterval(function() {
+        document.getElementById(`${key}-content`).innerHTML = info.join('\n');
+      }, 1 * 1000);
+    }
+
+  } else {
+
+    ret = (`
+      <div class="embed status" id="${key}">
+
+      <div class="titleBar">
+        <span class="titleText">${key}</span>
+        <span class="infoText">⏰ ${time}</span>
+      </div>
+
+      <div class="desc">
+        <span>${desc}</span>
+      </div>
+
+      <div class="embedContent" id="${key}-content">${info.join('\n')}</div>
+      </div>
+    `)
+
+  }
+
+
+  return ret;
+
 };
+
+
+
+function displayEmbed(data) {
+  var keys = Object.keys(data);
+  var returnHTML = '';
+  for(i=0;i<keys.length;i++) {
+    var key = keys[i];
+    returnHTML += embed(data[key], key, data[key]['last'], data[key]['desc'])
+  }
+  return(returnHTML)
+};
+
+
+
 
 
 function jsonDisplay(container, data) {
