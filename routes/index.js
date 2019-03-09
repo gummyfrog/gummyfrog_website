2d76a0497a0165c6892751503a333127ae5d66d6
gummyfrog <neilsemail@me.com>
2019-03-09 16:18:06 -0500

    sandaval 2020

--- routes/index.js
+++ routes/index.js
@@ -57,8 +57,8 @@ class customRouter {
       });
     });
 
-    this.router.get('/sandwich2020', function(req, res){
-      res.render('sandwich2020', {
+    this.router.get('/sandaval2020', function(req, res){
+      res.render('sandaval2020', {
         title: 'SANDWICH 2020'
       });
     });
