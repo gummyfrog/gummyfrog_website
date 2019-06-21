da8eacfae9c6646856984ab634b56ec60077d883
Neil <neilsemail@me.com>
2019-06-20 20:49:50 -0400

    snackbar

--- public/javascripts/cartesian.js
+++ public/javascripts/cartesian.js
@@ -8,6 +8,10 @@ var values = {
 	two: { vals: [500, 450, 400], name: "Two Columns" },
 }
 
+function f(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function cartesian(array) {
 	var results = [[]];
 
@@ -26,7 +30,7 @@ function cartesian(array) {
 	return results;
 }
 
-function break_even(available, profit, goal, valuations) {
+function break_even(available, goal, valuations) {
 	var permutable = [];
 	for(var i=0;i<available.length;i++) {
 		permutable.push(valuations[available[i]].vals);
@@ -37,45 +41,72 @@ function break_even(available, profit, goal, valuations) {
 		var sum = a.reduce((total, num)=>total + num);
 		return {combo: a, sum: sum}
 	})
-	.filter(a => a.sum >= goal-profit) // upper bound would be here
+	.filter(a => a.sum >= goal) // upper bound would be here
 	.sort((a, b) => b.sum-a.sum);
-	
-	console.log(available);
-	console.log(x);
 
 	if(x.length != 0) {
-		snack(`${x.length} results, the highest profit being $${x[0].sum} and the lowest being $${x[x.length-1].sum}.`)
+		var message = `Displaying ${f(x.length)} results, the highest profit being $${f(x[0].sum)} and the lowest being $${f(x[x.length-1].sum)}.`
+		SnackBar({message: message, status:"success"})
+		resultsInfo.innerHTML = message;
 	}
 
 	return(x);
 }
 
-document.getElementById("calculate").addEventListener("click", function(){
+
+// dom stuff
+
+let inputFields = document.querySelectorAll(".input-field");
+let resultsInfo = document.getElementById("resultsInfo")
+let formElement = document.getElementById('form');
+
+inputFields.forEach(el => {
+	if(el.id != "lower") {
+		el.addEventListener('input', getNoOfResults)
+	}
+})
+
+document.getElementById("calculate").addEventListener("click", calculate)
+
+
+function calculate() {
+	var form = getFormNumbers()
+	console.log(form);
+	var data = break_even(form.available, form.lower, values);
+	display(document.getElementById('jsonDisplay'), data, form.available, values)
+}
+
+function getNoOfResults() {
+	var form = getFormNumbers();
+	var results = Math.pow(values.full.vals.length, form.total);
+	var status = "info"
+	var ests = Math.round(Math.pow((1.2+(form.total) - (0.05*form.total))/4 , form.total*0.7))
+
+	if(results > 59049) {
+		status = "error"
+	}
+	SnackBar({message:`${f(results)} possible combinations exist. Time to display: ~${f(ests/100)}s`, status: status})
+}
+
+
+
+function getFormNumbers() {
 	var form = document.getElementById('form');
 	var available = [];
 	var lower = form[form.length-2].value
-
-	console.log(lower);
+	var total = 0;
 
 	for(var q=0;q<form.length-2;q++) {
-		if(form[q].value > 5) {
-			break;
+		if(form[q].value > 5 || form[q].value == "") {
+			continue
 		}
+
+		total += parseInt(form[q].value);
 
 		for(var b=0;b<parseInt(form[q].value);b++) {
 			available.push(form[q].id);
 		}
 	}
-	var data = break_even(available, 0, lower, values);
-
-	display(document.getElementById('jsonDisplay'), data, available, values)
-});
-
-
-function snack(message) {
-  var x = document.getElementById("snackbar");
-  x.innerHTML = message;
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
-} 
-
+	console.log(total);
+	return({available: available, lower: lower, total: total});
+}
--- public/javascripts/cartesianDisplayer.js
+++ public/javascripts/cartesianDisplayer.js
@@ -1,56 +1,33 @@
+
 function createTable(table, data, available, valuations) {
-  var header = table.insertRow(0);
-  var headerData = `<td><h2 class="tableheader"> profit </h2></td>`;
+  var tb = `<tr><td><h2 class="tableheader"> profit </h2></td>`;
 
   for(var h=0;h<available.length;h++) {
-    headerData += `<td><h2 class="tableheader">${available[h]}</h2></td>`;
+    tb += `<td><h2 class="tableheader">${available[h]}</h2></td>`;
   }
+  tb += "</tr>"
 
-  header.innerHTML = headerData;
+  // header row done
 
   for(var r=0;r<data.length;r++) {
-    var row = table.insertRow(r+1);
-    var rowData = `<td>$${f(data[r].sum)}</td>`
+    var rowData = `<tr class="tableRow"><td class="sum">$${f(data[r].sum)}</td>`
 
     for(var c=0;c<data[r].combo.length;c++) {
       var value = (valuations[available[c]].vals.indexOf(data[r].combo[c]));
-
       rowData += `<td class= "v${value}">$${f(data[r].combo[c])}</td>`;
     }
 
-    row.innerHTML = rowData;
+    tb += rowData+"</tr>"
   }
+
+  table.innerHTML = `<tbody>${tb}</tbody>`;
 }
-
-
-// function createTable(table, data, available) {
-//   var tb = `<tr><td><h2 class="tableheader"> sum </h2></td>`;
-
-//   for(var h=0;h<available.length;h++) {
-//     tb += `<td><h2 class="tableheader">${available[h]}</h2></td>`;
-//   }
-//   tb += "</tr>"
-
-//   // header row done
-
-//   for(var r=0;r<data.length;r++) {
-//     var rowData = `<tr><td>${data[r].sum}</td>`
-
-//     for(var c=0;c<data[r].combo.length;c++) {
-//       rowData += `<td>${data[r].combo[c]}</td>`;
-//     }
-
-//     tb += rowData+"</tr>"
-//   }
-
-//   table.innerHTML = `<tbody>${tb}</tbody>`;
-// }
-
-
 
 function display(container, data, available, values) {
   container.innerHTML = "";
+  var t0 = performance.now();
   createTable(container, data, available, values)
+  var t1 = performance.now();
 }
 
 
